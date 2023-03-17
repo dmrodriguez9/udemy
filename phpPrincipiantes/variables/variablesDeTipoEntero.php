a7e1afc5b591830b274a6184b8ca681140deea0c
dmrodriguez9 <dmrodriguez9@espe.edu.ec>
2023-03-17 15:52:47 -0500

    Actualizaciones

---
+++ phpPrincipiantes/variables/variablesDeTipoEntero.php
@@ -0,0 +1,16 @@
+<?php
+
+$numeroEnteroPositivo= 123;
+$numeroEnteroNegativo= -123;
+
+$numero1=10;
+$numero2=5;
+
+$suma=$numero1+$numero2;
+$resta=$numero1-$numero2;
+
+echo "El resultado de sumar " . $numero1 . "+" . $numero2 . " es: " . $suma;
+echo "<br>El resultado de restar " . $numero1 . "-" . $numero2 . " es: " . $resta;
+
+
+?>
---
+++ phpPrincipiantes/variables/variablesDeTipoFlotante.php
@@ -0,0 +1,15 @@
+<?php
+
+$numeroEnteroPositivo= 123.50;
+$numeroEnteroNegativo= -123.255;
+
+$numero1=10.23;
+$numero2=5.15;
+
+$suma=$numero1+$numero2;
+$resta=$numero1-$numero2;
+
+echo "El resultado de sumar " . $numero1 . "+" . $numero2 . " es: " . $suma;
+echo "<br>El resultado de restar " . $numero1 . "-" . $numero2 . " es: " . $resta;
+
+?>
