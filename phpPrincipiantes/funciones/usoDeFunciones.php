a3795592e63a671b9b22db5db178702dae7ba038
dmrodriguez9 <dmrodriguez9@espe.edu.ec>
2023-03-20 11:13:01 -0500

    Actualizaciones

---
+++ phpPrincipiantes/funciones/usoDeFunciones.php
@@ -0,0 +1,12 @@
+<?php
+
+function calcularArea($valor1, $valor2){
+
+    $resultado= ($valor1*$valor2)/2;
+    return $resultado;
+}
+
+echo calcularArea(3,4);
+
+?>
+
--- phpPrincipiantes/metodosGetPost/metodosGetPost.php
+++ phpPrincipiantes/metodosGetPost/metodosGetPost.php
@@ -1,4 +1,12 @@
-<form action="" method="get">
+<!--<form action="" method="get">
+    <label for="nombre">Nombre:</label>
+    <input type="text" id="nombre" name="nombre" placeholder="Ingrese su nombre"><br>
+    <br><label for="apellido">Apellido:</label>
+    <input type="text" id="apellido" name="apellido" placeholder="Ingrese su apellido"><br><br>
+    <input type="submit" value="Enviar">
+</form>-->
+
+<form action="" method="post">
     <label for="nombre">Nombre:</label>
     <input type="text" id="nombre" name="nombre" placeholder="Ingrese su nombre"><br>
     <br><label for="apellido">Apellido:</label>
@@ -8,6 +16,9 @@
 
 <?php
     //Para tomar las variables de la url hacemos uso del GET
-    echo "El nombre ingresado fue: " . $_GET["nombre"] . ", y el apellido fue: " . $_GET["apellido"];
-
+    //echo "El nombre ingresado fue: " . $_GET["nombre"] . ", y el apellido fue: " . $_GET["apellido"];
+    //Para tomar las variables hacemos del 
+    if(isset($_POST["nombre"]) && $_POST["apellido"]){
+        echo "El nombre ingresado fue: " . $_POST["nombre"] . ", y el apellido fue: " . $_POST["apellido"];
+    }
 ?>
