e4d08c8ff5c1623786c0b06b97d994ff64133de6
dmrodriguez9 <dmrodriguez9@espe.edu.ec>
2023-09-07 10:17:49 -0500

    python

--- desarrolloweb/python/Fundamentos/cadenas.py
+++ desarrolloweb/python/Fundamentos/cadenas.py
@@ -4,5 +4,5 @@ caracter=texto[0:4] #0:4 va desde la posicion 0 a la 4
 print(caracter)
 mayusculas=texto.upper() #upper convierte en mayusculas
 print(mayusculas)
-minusculas=texto.lower() #lower Convierte en minisculas
+minusculas=texto.lower() #lower nos ayuda a convertir en minisculas
 print(minusculas)
