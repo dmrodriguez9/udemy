457aec1e60cc5ddbd7ef8714fde61b84b51fa091
dmrodriguez9 <dmrodriguez9@espe.edu.ec>
2026-02-25 14:34:48 -0500

    .net proyecto api rest

---
+++ frameworks/back/.netCoreSqlServerSeguridadJwt/dotNET/CsBases/Fundamentals/Product.cs
@@ -0,0 +1,53 @@
+//Clases e interfaces
+using CsBases.Fundamentals;
+
+public class Product : IProduct
+{
+    public int Id {get; set;}
+    public string Name{get; set;}
+    public decimal Price { get; set;}
+    public DateTime CreateAT {get; set;}
+    public Guid UniqueCode {get; set;}
+
+    //Constructor
+    public Product (string name, decimal price)
+    {
+        Name= name;
+        Price = price;
+        CreateAT= DateTime.Now;
+        UniqueCode= Guid.NewGuid();
+    }
+    
+
+    //Metodos 
+
+    public void ApplyDiscount (decimal percentage)
+    {
+        var discountAmount= Price * (percentage/100);
+        Price -= discountAmount;
+    }
+
+    public virtual string GetDescription()  //virtual; nos servira para hacer herencia
+    {
+        return $"{Name} - {Price:C}";
+    }
+ 
+}
+
+//Nueva Class //Herencia
+
+class ServiceProduct: Product
+{
+    public int DurationInDays{get; set;}
+
+    public ServiceProduct(string name, decimal price, int duration): base(name, price)
+    {
+        DurationInDays= duration;
+    }
+
+    //Metodo
+    public override string GetDescription()
+    {
+        return $"{base.GetDescription()} - Duración: {DurationInDays} días ";
+    }
+}
---
+++ frameworks/back/.netCoreSqlServerSeguridadJwt/dotNET/CsBases/Program.cs
@@ -0,0 +1,29 @@
+﻿//Seccion 2 
+class Program
+{
+    static void Main()
+    {
+        //Console.WriteLine("Hello, World");
+        //El Console configurado en CsBases.csproj
+        //WriteLine("Hello, World");
+        
+        //Tipos Basicos
+        //Declaracion explicita 
+        int quantity= 5;
+        string message= "Hello World";
+        decimal price= 19.99m;
+        //:C, Formatea un número como moneda (Currency) según la configuración regional del sistema.
+        //WriteLine($"Cantidad: {quantity}, Saludo: {message}, Precio: {price:C}");
+
+        //Declaración con var
+        var gretting = "Hola";
+        var percentage= 20.00m;
+        //WriteLine($"Saludo: {gretting}, Porcentaje: {percentage:C}");
+
+        //Creando un objeto de la clase producto
+        var laptop= new Product("Laptop", 1200);
+        WriteLine(laptop.GetDescription());
+        var soporte = new ServiceProduct("Soporte técnico", 300, 300);
+        Write(soporte.GetDescription());
+    }
+}
