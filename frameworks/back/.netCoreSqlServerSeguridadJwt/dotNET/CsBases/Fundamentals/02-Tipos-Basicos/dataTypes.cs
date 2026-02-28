a1ea6f18f3ee058d647e3a2ad785893f47a7cd46
dmrodriguez9 <dmrodriguez9@espe.edu.ec>
2026-02-28 12:42:00 -0500

    organizacion por carpetas

---
+++ frameworks/back/.netCoreSqlServerSeguridadJwt/dotNET/CsBases/Fundamentals/02-Tipos-Basicos/dataTypes.cs
@@ -0,0 +1,19 @@
+class DataTypes
+{
+     //Console.WriteLine("Hello, World");
+        //El Console configurado en CsBases.csproj
+        //WriteLine("Hello, World");
+        
+        //Tipos Basicos
+        //Declaracion explicita 
+        //int quantity= 5;
+        //string message= "Hello World";
+        //decimal price= 19.99m;
+        //:C, Formatea un número como moneda (Currency) según la configuración regional del sistema.
+        //WriteLine($"Cantidad: {quantity}, Saludo: {message}, Precio: {price:C}");
+
+        //Declaración con var
+        //var gretting = "Hola";
+        //var percentage= 20.00m;
+        //WriteLine($"Saludo: {gretting}, Porcentaje: {percentage:C}");
+}
