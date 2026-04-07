208d3763e1f5871b948bd56d28cbb08f6f918a2b
dmrodriguez9 <dmrodriguez9@espe.edu.ec>
2026-04-07 12:47:46 -0500

    Dto y Controller

---
+++ frameworks/back/.netCoreSqlServerSeguridadJwt/dotNET/ApiEcommerce/Mapping/CategoryProfile.cs
@@ -0,0 +1,13 @@
+using ApiEcommerce.Models.Dtos;
+using AutoMapper;
+
+namespace ApiEcommerce.Mapping;
+
+public class CategoryProfile: Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<Category, CategoryDto>().ReverseMap();
+        CreateMap<Category, CreateCategoryDto>().ReverseMap();
+    }
+}
